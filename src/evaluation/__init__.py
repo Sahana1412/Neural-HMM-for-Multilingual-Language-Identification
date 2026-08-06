ae2cdf19394e33f5505d6c08f838c1400f6598e2
Sahana R <sahanarameshkumar1@gmail.com>
2026-08-06 16:57:09 +0530

    Initialize evaluation module with metrics and report

---
+++ src/evaluation/__init__.py
@@ -0,0 +1,8 @@
+"""Evaluation metrics and reporting."""
+
+from src.evaluation.metrics import LanguageIDMetrics, format_metrics_report
+
+__all__ = [
+    "LanguageIDMetrics",
+    "format_metrics_report",
+]
